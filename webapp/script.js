a92704d4a3861496e27715e53905d10c03e67045
CookieMeo <149961732+CookieMeo@users.noreply.github.com>
2026-02-21 13:55:47 +0300

    Update script.js

--- webapp/script.js
+++ webapp/script.js
@@ -20,7 +20,7 @@ async function updateUI() {
     const u = await api('/get_user', {user_id: uid});
     
     // Обновление валюты
-    document.querySelectorAll('.straw-count').forEach(el => el.innerText = u.strawberry);
+    document.querySelectorAll('.strawberry-count').forEach(el => el.innerText = u.strawberry);
     document.querySelectorAll('.spins-count').forEach(el => el.innerText = u.spins);
     
     // Гаранты
@@ -82,5 +82,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     updateUI();
 });
+
 
 
